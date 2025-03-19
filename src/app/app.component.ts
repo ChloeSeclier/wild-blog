55640ff5b5068feaed8ced339f1c7b9a2fa40622
Chloe Seclier <seclier.chloe@gmail.com>
2025-03-19 10:59:56 +0100

    add article list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ArticleListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Bienvenue sur le Wild Blog de Chloé';
