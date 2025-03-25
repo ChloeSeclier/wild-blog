871178d074cedaadbe35b5faabbf0a9873eab381
Chloe Seclier <seclier.chloe@gmail.com>
2025-03-25 11:04:32 +0100

    quete 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ArticleListComponent } from './components/article-list/article-list.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { ArticlePageComponent } from './pages/article-page/article-page.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ArticleListComponent],
+  imports: [HeaderComponent, CommonModule, RouterOutlet, ArticleListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { ArticlePageComponent} from './pages/article-page/article-page.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: HomePageComponent},
+    {path:'contact', component:ContactPageComponent},
+    {path:'article/:id', component:ArticlePageComponent},
+    {path:'**', component:NotFoundComponent}
+];
