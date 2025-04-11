b4669ee6affbe60928a6df83feac45ec3be13bce
Chloe Seclier <seclier.chloe@gmail.com>
2025-04-11 14:29:14 +0200

    quete 6 ok

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from "../../components/contact-form/contact-form.component";
 
 @Component({
   selector: 'app-contact-page',
-  imports: [],
+  imports: [ContactFormComponent],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
