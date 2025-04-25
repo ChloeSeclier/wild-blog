a9ad9b1938be4478485f308deb6d89672b6846b2
Chloe Seclier <seclier.chloe@gmail.com>
2025-04-25 11:15:18 +0200

    quete8

--- src/app/components/article-list/article-list.component.ts
+++ src/app/components/article-list/article-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import Article from '../../models/article.model';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { ArticleThumbnailComponent } from '../article-thumbnail/article-thumbnail.component';
 
 @Component({
   selector: 'app-article-list',
-  imports: [CommonModule, RouterLink],
+  standalone: true,
+  imports: [CommonModule, ArticleThumbnailComponent],
   templateUrl: './article-list.component.html',
   styleUrl: './article-list.component.scss',
 })
---
+++ src/app/components/article-thumbnail/article-thumbnail.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import Article from '../../models/article.model';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-article-thumbnail',
+  standalone: true,
+  imports: [RouterLink, CommonModule],
+  templateUrl: './article-thumbnail.component.html',
+  styleUrl: './article-thumbnail.component.scss',
+})
+export class ArticleThumbnailComponent {
+  @Input() article!: Article;
+}
