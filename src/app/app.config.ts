6fd5b33c47ad2bd38371104f1c967d2cf3038b0f
Chloe Seclier <seclier.chloe@gmail.com>
2025-05-04 20:49:49 +0200

    test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,37 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import Article from './models/article.model';
+import { map, Subscription } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+  ],
 };
+
+export class ExampleComponent {
+
+  articles: Article[] = [];
+  articleSubscription: Subscription;
+
+  http = inject(HttpClient); 
+
+  ngOnInit() {
+    this.articleSubscription = this.http.get<Article[]>('http://api.exemple.com/articles').subscribe(data => {
+      this.articles = data;
+    });
+  }
+
+  ngOnDestroy() {
+    this.articleSubscription.unsubscribe();
+  }
+}
