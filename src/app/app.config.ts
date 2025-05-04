aa23069e7c3beba5404127633b2fc994808e6872
Chloe Seclier <seclier.chloe@gmail.com>
2025-05-04 21:57:43 +0200

    quest 10

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,30 +8,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
 import Article from './models/article.model';
-import { map, Subscription } from 'rxjs';
+import { map, Observable, Subscription } from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideHttpClient(),
   ],
 };
-
-export class ExampleComponent {
-
-  articles: Article[] = [];
-  articleSubscription: Subscription;
-
-  http = inject(HttpClient); 
-
-  ngOnInit() {
-    this.articleSubscription = this.http.get<Article[]>('http://api.exemple.com/articles').subscribe(data => {
-      this.articles = data;
-    });
-  }
-
-  ngOnDestroy() {
-    this.articleSubscription.unsubscribe();
-  }
-}
--- src/app/pages/article-page/article-page.component.ts
+++ src/app/pages/article-page/article-page.component.ts
@@ -1,20 +1,28 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs';
+import Article from '../../models/article.model';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
+import { ArticleThumbnailComponent } from '../../components/article-thumbnail/article-thumbnail.component';
 
 @Component({
   selector: 'app-article-page',
-  imports: [],
+  imports: [CommonModule, ArticleThumbnailComponent],
   templateUrl: './article-page.component.html',
   styleUrl: './article-page.component.scss',
 })
 export class ArticlePageComponent implements OnInit {
   articleId!: number;
+  articles$! : Observable<Article>;
 
   route: ActivatedRoute = inject(ActivatedRoute);
+  apiService = inject(ApiService);
 
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.articleId = Number(params.get('id'));
+      this.articles$=this.apiService.getArticleById(this.articleId);  
     });
   }
 }
